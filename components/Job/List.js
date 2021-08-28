4e0bfa634f90ecf0d54a335ee401c5d2093c69a9
Michael Peters <michaelpeters5647@gmail.com>
2021-08-28 17:56:58 +0200

    Add input and button

--- components/Job/List.js
+++ components/Job/List.js
@@ -1,6 +1,8 @@
 export default function JobList({ children }) {
   return (
     <div>
+      <input type="text" placeholder="CRON" />
+      <button>Add Job</button>
       <h1>Jobs</h1>
       {children}
     </div>
