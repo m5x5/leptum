7150900ebae905af599892cac1a89c80832dd3c0
Michael Peters <michaelpeters5647@gmail.com>
2021-08-29 10:45:57 +0200

    Convert list cron names to natural language

--- components/Job/ListItem.js
+++ components/Job/ListItem.js
@@ -1,16 +1,20 @@
 import { useJobContext } from "./Context";
 import { ChevronDoubleUpIcon } from "@heroicons/react/solid";
+import cronstrue from "cronstrue";
 
 // Create tailwind list item
 export default function JobListItem({ job } = { job: {} }) {
   const { setSelected } = useJobContext();
+  if (!job.cron) return null;
+  const descriptiveName = cronstrue.toString(job.cron);
+
   const onClick = () => {
     setSelected(job.cron);
   };
 
   return (
     <div className="flex justify-between" onClick={onClick}>
-      <h3 className="inline">{job.cron}</h3>
+      <h3 className="inline">{descriptiveName}</h3>
       {job.status === "pending" ? (
         <ChevronDoubleUpIcon className="inline text-blue-500 h-4 w-4 m-1" />
       ) : (
