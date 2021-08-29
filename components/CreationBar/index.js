2c911c7e6c1a714944976dcbdc54b469a73f9ca6
Michael Peters <michaelpeters5647@gmail.com>
2021-08-29 14:20:44 +0200

    Make form submit easier

--- components/CreationBar/index.js
+++ components/CreationBar/index.js
@@ -29,19 +29,19 @@ export default function CreationBar() {
   };
 
   return (
-    <form onSubmit={onSubmit}>
-      <div className="flex items-center mt-5">
-        <button
-          className="bg-white p-3 rounded-l-xl"
-          onClick={onSwitchMode}
-          onSubmit={false}
-        >
-          {mode === "task" ? (
-            <PencilIcon className="w-5 h-5 text-gray-400" />
-          ) : (
-            <ClockIcon className="w-5 h-5 text-gray-400" />
-          )}
-        </button>
+    <div className="flex items-center mt-5">
+      <button
+        className="bg-white p-3 rounded-l-xl"
+        onClick={onSwitchMode}
+        onSubmit={false}
+      >
+        {mode === "task" ? (
+          <PencilIcon className="w-5 h-5 text-gray-400" />
+        ) : (
+          <ClockIcon className="w-5 h-5 text-gray-400" />
+        )}
+      </button>
+      <form onSubmit={onSubmit} className="flex flex-row">
         <input
           className="bg-white focus:border-blue-500 p-3 rounded-r-xl appearance-none border-2 border-gray-200 w-full text-gray-700 leading-tight focus:outline-none"
           id="todo"
@@ -57,7 +57,7 @@ export default function CreationBar() {
         >
           <PlusIcon className="w-6 h-6" />
         </button>
-      </div>
-    </form>
+      </form>
+    </div>
   );
 }
