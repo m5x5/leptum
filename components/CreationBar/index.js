139c3201397cfbf34a00ac1e75996988a616f276
Michael Peters <michaelpeters5647@gmail.com>
2021-08-29 14:14:51 +0200

    Replace other input boxes with main one

--- components/CreationBar/index.js
+++ components/CreationBar/index.js
@@ -1,19 +1,41 @@
 import { PencilIcon, PlusIcon, ClockIcon } from "@heroicons/react/outline";
 import { useState } from "react";
+import { useJobContext } from "../Job/Context";
 
 // Create add todo form with tailwindcss
 export default function CreationBar() {
+  const { addTask, addJob } = useJobContext();
   const [mode, setMode] = useState("task");
+  const [text, setText] = useState("");
 
   const onSwitchMode = (e) => {
     e.preventDefault();
     setMode(mode === "task" ? "cron" : "task");
   };
 
+  const onChangeText = (e) => {
+    setText(e.target.value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (!text) return;
+    if (mode === "task") {
+      addTask(text);
+    } else {
+      addJob(text);
+    }
+    setText("");
+  };
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <div className="flex items-center mt-5">
-        <button className="bg-white p-3 rounded-l-xl" onClick={onSwitchMode}>
+        <button
+          className="bg-white p-3 rounded-l-xl"
+          onClick={onSwitchMode}
+          onSubmit={false}
+        >
           {mode === "task" ? (
             <PencilIcon className="w-5 h-5 text-gray-400" />
           ) : (
@@ -24,11 +46,14 @@ export default function CreationBar() {
           className="bg-white focus:border-blue-500 p-3 rounded-r-xl appearance-none border-2 border-gray-200 w-full text-gray-700 leading-tight focus:outline-none"
           id="todo"
           type="text"
-          placeholder="Add a new todo"
+          placeholder={mode === "task" ? "Add a task" : "Add a CRON job"}
+          value={text}
+          onChange={onChangeText}
         />
         <button
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold p-2 rounded-xl focus:outline-none focus:shadow-outline"
           type="button"
+          onClick={onSubmit}
         >
           <PlusIcon className="w-6 h-6" />
         </button>
--- components/Tasks/List.js
+++ components/Tasks/List.js
@@ -3,46 +3,5 @@ import { useState } from "react";
 import { useJobContext } from "../Job/Context";
 
 export default function TaskList({ children }) {
-  const [task, setTask] = useState([]);
-  const { addTask, updateJob, jobIndex } = useJobContext();
-
-  const onChange = (e) => {
-    const { value } = e.target;
-    setTask(value);
-  };
-
-  const onSubmit = (e) => {
-    e.preventDefault();
-    addTask(task);
-    setTask("");
-  };
-
-  const onDone = (e) => {
-    e.preventDefault();
-    updateJob({ status: "due" });
-  };
-
-  return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          name="task"
-          onChange={onChange}
-          value={task}
-          placeholder="Task name"
-        />
-        <button className="bg-gray-400 hover:bg-gray-500 active:bg-gray-600 m-1">
-          <PlusIcon className="w-4 h-4 text-gray-100" />
-        </button>
-        <button
-          className="bg-gray-400 hover:bg-gray-500 active:bg-gray-600 m-1"
-          onClick={onDone}
-        >
-          <CheckIcon className="w-4 h-4 text-gray-100" />
-        </button>
-      </form>
-      {children}
-    </div>
-  );
+  return <div>{children}</div>;
 }
