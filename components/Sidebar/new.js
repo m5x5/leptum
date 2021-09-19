b008494c7c3a40c2cd9488c42e160992395ba26b
Michael Peters <michaelpeters5647@gmail.com>
2021-09-19 17:05:58 +0200

    Add links to sidebar

--- components/Sidebar/new.js
+++ components/Sidebar/new.js
@@ -1,6 +1,9 @@
+import { ClockIcon, TerminalIcon } from "@heroicons/react/outline";
+import Link from "next/link";
+
 export default function NewSidebar() {
   return (
-    <div className="h-screen w-14 bg-black p-1 inline-block">
+    <div className="h-screen w-14 bg-black p-1 flex flex-col">
       <div className="px-4 py-1 w-12 h-12 rounded-md select-none">
         <span
           className="text-white text-3xl"
@@ -8,6 +11,16 @@ export default function NewSidebar() {
         >
           L
         </span>
+      </div>
+      <div className="flex-grow flex flex-col items-center justify-center">
+        <Link href="/">
+          <div className="text-gray-400 p-3 hover:text-blue-600 transition cursor-pointer">
+            <ClockIcon className="h-7" />
+          </div>
+        </Link>
+        <div className="text-gray-400 p-3 hover:text-blue-600 transition cursor-pointer">
+          <TerminalIcon className="h-7" />
+        </div>
       </div>
     </div>
   );
