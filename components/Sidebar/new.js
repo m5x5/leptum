a9d612717914bcea5a9f0d6ab821f5bf607b5643
Michael Peters <michaelpeters5647@gmail.com>
2021-09-19 16:52:11 +0200

    Better sidebar

--- components/Sidebar/new.js
+++ components/Sidebar/new.js
@@ -1,8 +1,13 @@
 export default function NewSidebar() {
   return (
     <div className="h-screen w-14 bg-black p-1 inline-block">
-      <div className="px-4 py-1 bg-gray-800 w-12 h-12 rounded-md select-none">
-        <span className="text-white text-3xl">L</span>
+      <div className="px-4 py-1 w-12 h-12 rounded-md select-none">
+        <span
+          className="text-white text-3xl"
+          style={{ fontFamily: "Rye", fontWeight: "regular" }}
+        >
+          L
+        </span>
       </div>
     </div>
   );
