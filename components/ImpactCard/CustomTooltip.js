654e9ced771224a8c8a6a65ae92dbb9363cbf258
Michael Peters <michaelpeters5647@gmail.com>
2021-09-20 19:50:11 +0200

    Better function structure

--- components/ImpactCard/CustomTooltip.js
+++ components/ImpactCard/CustomTooltip.js
@@ -1,26 +1,25 @@
 import { capitalize } from "../../utils";
 
 export default function ({ active, payload, label }) {
-  if (active && payload && payload.length) {
-    const item = payload[0];
-    return (
-      <div className="bg-gray-800 p-4 rounded-lg">
-        <div className="text-lg">{item.payload.activity}</div>
-        <div className="tooltip-content">
-          <ul>
-            <li key={item.id} className="flex items-center justify-start pt-1">
-              <span
-                style={{ backgroundColor: item.fill }}
-                className="h-5 w-5 inline-block rounded-lg m-1"
-              ></span>
-              <span className="">
-                {capitalize(item.dataKey)}: {item.value}
-              </span>
-            </li>
-          </ul>
-        </div>
+  if (!(active && payload && payload.length)) return null;
+
+  const item = payload[0];
+  return (
+    <div className="bg-gray-800 p-4 rounded-lg">
+      <div className="text-lg">{item.payload.activity}</div>
+      <div className="tooltip-content">
+        <ul>
+          <li key={item.id} className="flex items-center justify-start pt-1">
+            <span
+              style={{ backgroundColor: item.fill }}
+              className="h-5 w-5 inline-block rounded-lg m-1"
+            ></span>
+            <span className="">
+              {capitalize(item.dataKey)}: {item.value}
+            </span>
+          </li>
+        </ul>
       </div>
-    );
-  }
-  return null;
+    </div>
+  );
 }
