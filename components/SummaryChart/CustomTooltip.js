4a7c213bc2f912fc00885b1bc6a188761a16c47c
Michael Peters <michaelpeters5647@gmail.com>
2021-09-20 19:53:56 +0200

    Better function structure

--- components/SummaryChart/CustomTooltip.js
+++ components/SummaryChart/CustomTooltip.js
@@ -1,34 +1,32 @@
 import { capitalize } from "../../utils";
 
-export default function ({ active, payload, label }) {
-  if (active && payload && payload.length) {
-    console.log({ active, payload, label });
-    return (
-      <div className="bg-gray-800 p-4 rounded-lg">
-        <div className="text-lg">{payload[0].payload.activity}</div>
-        <div className="tooltip-content">
-          <ul>
-            {payload
-              .filter((item) => +item.value)
-              .reverse()
-              .map((item) => (
-                <li
-                  key={item.id}
-                  className="flex items-center justify-start pt-1"
-                >
-                  <span
-                    style={{ backgroundColor: item.fill }}
-                    className="h-5 w-5 inline-block rounded-lg m-1"
-                  ></span>
-                  <span className="">
-                    {capitalize(item.dataKey)}: {item.value}
-                  </span>
-                </li>
-              ))}
-          </ul>
-        </div>
+export default function CustomTooltip({ active, payload, label }) {
+  if (!(active && payload && payload.length)) return null;
+  console.log({ active, payload, label });
+  return (
+    <div className="bg-gray-800 p-4 rounded-lg">
+      <div className="text-lg">{payload[0].payload.activity}</div>
+      <div className="tooltip-content">
+        <ul>
+          {payload
+            .filter((item) => +item.value)
+            .reverse()
+            .map((item) => (
+              <li
+                key={item.id}
+                className="flex items-center justify-start pt-1"
+              >
+                <span
+                  style={{ backgroundColor: item.fill }}
+                  className="h-5 w-5 inline-block rounded-lg m-1"
+                ></span>
+                <span className="">
+                  {capitalize(item.dataKey)}: {item.value}
+                </span>
+              </li>
+            ))}
+        </ul>
       </div>
-    );
-  }
-  return null;
+    </div>
+  );
 }
