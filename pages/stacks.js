3d7274cb00a9bcf51c780e61f9670ef894418ecf
Michael Peters <michaelpeters5647@gmail.com>
2021-09-21 16:40:58 +0200

    Add Stacks

--- pages/stacks.js
+++ pages/stacks.js
@@ -14,6 +14,17 @@ const defaultStacks = [
       {
         name: "Put mobile in my shelf",
       },
+      {
+        name: "Set Goals for the time at home",
+      },
+    ],
+  },
+  {
+    name: "Eat Dinner",
+    habits: [
+      {
+        name: "Clean Dishes",
+      },
     ],
   },
 ];
@@ -25,17 +36,22 @@ export default function StacksPage() {
     <div className="w-full h-full bg-gray-900 text-white flex">
       <NewSidebar />
       <div className="flex-grow p-4 h-screen overflow-y-auto">
-        <h1 className="text-2xl">Stacks</h1>
-        {stacks.map((stack) => (
-          <div className="pa-4" key={stack.id}>
-            <h2 className="text-xl text-white mb-4">{stack.name}</h2>
-            {stack.habits.map((habit) => (
-              <div className="pa-2" key={habit.id}>
-                <h3 className="text-lg text-white mb-4">{habit.name}</h3>
-              </div>
-            ))}
-          </div>
-        ))}
+        <h1 className="text-2xl mb-5">Stacks</h1>
+        <div className="grid grid-cols-2 gap-4">
+          {stacks.map((stack, index) => (
+            <div
+              className="p-4 bg-gray-800 rounded-lg"
+              key={(stack.name, index)}
+            >
+              <h2 className="text-xl text-white mb-4">{stack.name}</h2>
+              {stack.habits.map((habit, index) => (
+                <div className="p-2 bg-gray-700" key={habit.name + index}>
+                  <h3 className="text-lg text-gray-300 mb-4">{habit.name}</h3>
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
