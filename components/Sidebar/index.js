d53ed3a0234acc272707e875ffd1441f7adfea26
Michael Peters <michaelpeters5647@gmail.com>
2021-09-25 07:45:20 +0200

    Add job names

--- components/Sidebar/index.js
+++ components/Sidebar/index.js
@@ -1,7 +1,18 @@
 import { useEffect, useState } from "react";
+import { PlusIcon } from "@heroicons/react/solid";
+import CreateJobModal from "../Modal/CreateJobModal";
 
 export default function Sidebar({ children, className, ...props }) {
   const [_time, setTime] = useState(Date.now());
+  const [showModal, setShowModal] = useState(false);
+
+  const onClick = () => {
+    setShowModal(true);
+  };
+
+  const hideModal = () => {
+    setShowModal(false);
+  };
 
   useEffect(() => {
     const interval = setInterval(() => setTime(Date.now()), 1000);
@@ -11,8 +22,9 @@ export default function Sidebar({ children, className, ...props }) {
   }, []);
 
   return (
-    <div
-      className={`
+    <>
+      <div
+        className={`
         flex-none
         flex-shrink-0
         bg-gray-100
@@ -26,9 +38,18 @@ export default function Sidebar({ children, className, ...props }) {
         max-h-60
         ${className}
       `}
-      {...props}
-    >
-      {children}
-    </div>
+        {...props}
+      >
+        <div className="flex flex-row w-full justify-between items-center px-5 mb-3">
+          <p className="text-xl">CRON Jobs</p>
+          <PlusIcon
+            className="w-6 text-gray-500 cursor-pointer"
+            onClick={onClick}
+          />
+        </div>
+        {children}
+      </div>
+      <CreateJobModal isOpen={showModal} onHide={hideModal} />
+    </>
   );
 }
